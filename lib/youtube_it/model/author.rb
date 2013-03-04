364f82b4730b14f4cad5abba4ed89032c89c3e5c
Kevin J. Hanna <kevinjackhanna@gmail.com>
2013-03-04 14:57:04 -0300

    Retrieve video uploader's user id when searching

--- lib/youtube_it/model/author.rb
+++ lib/youtube_it/model/author.rb
@@ -1,12 +1,15 @@
 class YouTubeIt
   module Model
     class Author < YouTubeIt::Record
+      # *String*: Author's YouTube id.
+      attr_reader :user_id
+
       # *String*: Author's YouTube username.
       attr_reader :name
-      
+
       # *String*: Feed URL of the author.
       attr_reader :uri
-      
+
       attr_reader :thumbnail_url
     end
   end
